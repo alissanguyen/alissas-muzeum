57565129ca82a05a6e015ebdd7b72c5f9c4fee32
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-12 23:08:06 -0700

    Link database to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,39 @@
-import Image from "next/image";
-import Link from "next/link";
 import { db } from "~/server/db";
 
-const mockUrls: string[] = [
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-  "https://utfs.io/f/f5815374-5070-463e-9386-a48cb94af517-1x475r.png",
-  "https://utfs.io/f/84cdd2eb-3e53-4b23-88c1-c4fe6d4048f5-1x475u.png",
-  "https://utfs.io/f/a3f2e009-7bd2-4cda-a131-02f014dece26-1x475w.png",
-  "https://utfs.io/f/6b69e506-6925-403b-9527-07293e6bf333-1x475v.png",
-  "https://utfs.io/f/313df38c-0af5-4f0e-b2ae-a5f4d04944d5-1x475x.png",
-  "https://utfs.io/f/6b161ac7-798e-40f9-999d-9c2d57f4dd7c-bihvxr.png",
-  "https://utfs.io/f/5f6ee4a4-af67-46ca-9a3f-ad2a1bb6ebd7-1x475t.png",
-]
 
-const mockImages = mockUrls.map((url, index) => {
-  return {
-    id: index + 1,
-    url
-  }
-})
+
+export const dynamic = "force-dynamic"
+
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts)
+  /**
+   * The function (model, { desc }) => desc(model.id) is a callback function 
+   * passed to orderBy. Here, model represents each image record and { desc } 
+   * is a destructured object that contains sorting functions. The desc 
+   * function is used to sort in descending order.
+   * The desc function comes from the Prisma Client API. Prisma provides asc 
+   * and desc functions for ascending and descending sorting respectively. 
+   * In this case, desc(model.id) means "sort by the id field of the model 
+   * in descending order".
+   */
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id) 
+  });
   
+
+
+  console.log(images)
+
   return (
     <main className="p-10 w-full">
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5 w-full">
-        {mockImages.map((image) =>
-          <div className="mx-auto" key={image.id}><img src={image.url} alt="" className="w-48" /></div>)}
+        {images.map((image) =>
+          <div className="mx-auto text-center flex flex-col" key={image.id}><img src={image.url} alt="" className="w-full" /><span>{image.name}</span></div>)}
+        {images.map((image) =>
+          <div className="mx-auto text-center flex flex-col" key={image.id}><img src={image.url} alt="" className="w-full" /><span>{image.name}</span></div>)}
+        {images.map((image) =>
+          <div className="mx-auto text-center flex flex-col" key={image.id}><img src={image.url} alt="" className="w-full" /><span>{image.name}</span></div>)}
       </div>
     </main>
   );
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -9,7 +9,6 @@ import {
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
-
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
  * database instance for multiple projects.
@@ -18,11 +17,14 @@ import {
  */
 export const createTable = pgTableCreator((name) => `alissasmuzeum-postgres_${name}`);
 
-export const posts = createTable(
-  "post",
+
+
+export const images = createTable(
+  "image",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
