28f5ee0226efab15442f30e88abaa96c29e64b90
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-13 21:16:46 -0700

    add styles and clean up

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -6,9 +6,13 @@ export default async function FullPageImageView(props: { id: number }) {
     const image = await getImageById(props.id);
 
     return (
-        <>
-            <img src={image.url} alt={image.name} className="w-96" />
-            <p>{image.name}</p>
-        </>
+        <div className="flex w-full h-full p-10">
+            <div className="flex max-w-screen-xl mx-auto h-full bg-gray-200 p-20 gap-10 rounded-xl">
+                <img src={image.url} alt={image.name} className="max-w-[1/2] rounded-lg object-contain" />
+                <div className="flex flex-col">
+                    <p className="text-2xl text-black font-semibold">{image.name}</p>
+                </div>
+            </div>
+        </div>
     );
 }
