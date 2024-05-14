bfbc347b32a8ea8b258d3053ab096bd3d37991ee
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-13 22:04:12 -0700

    Setup shadui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,12 @@ export interface ImageResponse {
 
 function Images({ images }: { images: ImageResponse[] }) { // Correct the type annotation for the images parameter
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5 w-full justify-center p-10 pb-20">
+    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 gap-y-20 gap-5 w-full justify-center p-10 pb-20">
 
       {images.map((image) =>
-        <div className="mx-auto text-center flex flex-col" key={image.id}>
+        <div className="mx-auto text-center flex flex-col gap-2" key={image.id}>
           <Link href={`/img/${image.id}`} className="w-full h-full">
-            <Image src={image.url} alt="" height={800} width={600} className="Image rounded-xl"/>
+            <Image src={image.url} alt="" height={800} width={600} className="Image rounded-lg"/>
           </Link>
           <span>{image.name}</span>
 
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -10,7 +10,7 @@ export default async function FullPageImageView(props: { id: number }) {
 
     return (
         <div className="flex w-full h-full p-10">
-            <div className="flex max-w-screen-xl mx-auto h-full bg-gray-200 p-20 gap-10 rounded-xl">
+            <div className="flex max-w-screen-xl mx-auto h-full bg-slate-50 p-20 gap-10 rounded-xl">
                 <img src={image.url} alt={image.name} className="max-w-[1/2] rounded-lg object-contain" />
                 <div className="flex flex-col gap-3">
                     <p className="text-2xl text-black font-semibold">{image.name}</p>
