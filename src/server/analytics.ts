61e361a2b1d5b3b844d222776d02f482d442d8f0
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-14 17:11:36 -0700

    Ensure analytics.ts only runs on server

--- src/server/analytics.ts
+++ src/server/analytics.ts
@@ -1,11 +1,11 @@
+import "server-only";
 import { PostHog } from "posthog-node";
 
 export function serverSideAnalytics() {
-
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    flushAt: 1,             //The number of events to queue before sending to PostHog (flushing)
-    flushInterval: 0,       //The interval in milliseconds between periodic flushes
+    flushAt: 1, //The number of events to queue before sending to PostHog (flushing)
+    flushInterval: 0, //The interval in milliseconds between periodic flushes
   });
 
   return posthogClient;
